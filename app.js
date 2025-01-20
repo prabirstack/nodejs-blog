1fbe57e04698b93d5e372679707276f4e7aea633
Prabir <prabirsacademy@gmail.com>
2025-01-20 19:32:27 +0530

    fetching posts

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
+// database connection import
+const connectDB = require("./server/config/db");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// connect to database
+connectDB();
 
 //public folder
 app.use(express.static("public"));
@@ -14,6 +20,7 @@ app.set("view engine", "ejs");
 app.use(expressLayout);
 app.set("layout", "./layouts/main");
 
+app.get("/post/:id", require("./server/routes/main.routes"));
 app.get("/about", require("./server/routes/main.routes"));
 app.get("/contact", require("./server/routes/main.routes"));
 app.get("/", require("./server/routes/main.routes"));
